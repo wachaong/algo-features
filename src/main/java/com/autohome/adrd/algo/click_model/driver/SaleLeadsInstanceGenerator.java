4fd70a5abf5f08ba100ece6fa772640b3c82777b
wangchao <wachaong@gmail.com>
2014-10-13 08:54:15 +0800

    test

--- src/main/java/com/autohome/adrd/algo/click_model/driver/SaleLeadsInstanceGenerator.java
+++ src/main/java/com/autohome/adrd/algo/click_model/driver/SaleLeadsInstanceGenerator.java
@@ -16,6 +16,7 @@ import com.autohome.adrd.algo.click_model.driver.CtrInstanceGenerator.RCFileMapp
 import com.autohome.adrd.algo.click_model.io.AbstractProcessor;
 import com.autohome.adrd.algo.protobuf.AdLogOperation;
 import com.autohome.adrd.algo.protobuf.ApplogOperation;
+import com.autohome.adrd.algo.protobuf.BehaviorInfoOperation;
 import com.autohome.adrd.algo.protobuf.PvlogOperation;
 import com.autohome.adrd.algo.protobuf.SaleleadsInfoOperation;
 import com.autohome.adrd.algo.protobuf.TargetingKVOperation;
@@ -47,107 +48,111 @@ public class SaleLeadsInstanceGenerator extends AbstractProcessor {
 			List<PvlogOperation.AutoPVInfo> pvList = new ArrayList<PvlogOperation.AutoPVInfo>();
 			List<ApplogOperation.AutoAppInfo> app_pvList = new ArrayList<ApplogOperation.AutoAppInfo>();
 			List<SaleleadsInfoOperation.SaleleadsInfo> saleleadsList = new ArrayList<SaleleadsInfoOperation.SaleleadsInfo>();
-
+			List<BehaviorInfoOperation.BehaviorInfo> behaviorList = new ArrayList<BehaviorInfoOperation.BehaviorInfo>();
 			decode(key, value);
 
 			pvList = (List<PvlogOperation.AutoPVInfo>) list.get(CG_PV);
 			app_pvList = (List<ApplogOperation.AutoAppInfo>) list.get(CG_APPPV);
 			saleleadsList = (List<SaleleadsInfoOperation.SaleleadsInfo>) list.get(CG_SALE_LEADS);
-
+			behaviorList = (List<BehaviorInfoOperation.BehaviorInfo>) list.get(CG_BEHAVIOR);
 			TargetingKVOperation.TargetingInfo targeting = (TargetingKVOperation.TargetingInfo) list.get(CG_TAGS);
 			
-			HashSet<String> clk_set = new HashSet<String>();
+
 		
-			Sample pos = new Sample();
-			Sample neg = new Sample();
-			pos.setLabel(1.0);
-			neg.setLabel(0.0);
+
+			Sample sample = new Sample();
+
+			
 			if (targeting != null) {
 				if (targeting.getSeriesListCount() != 0) {
-					for (int i = 0; i < targeting.getSeriesListList().size(); i++) {
-
-						pos.setFeature("targetingSeriesid@"+targeting.getSeriesListList().get(i).getSeriesid(), targeting.getSeriesListList().get(i).getScore());
-						neg.setFeature("targetingSeriesid@"+targeting.getSeriesListList().get(i).getSeriesid(), targeting.getSeriesListList().get(i).getScore());
-						
+					for (int i = 0; i < targeting.getSeriesListList().size(); i++) {		
+						sample.setFeature("targetingSeriesid@"+targeting.getSeriesListList().get(i).getSeriesid(), targeting.getSeriesListList().get(i).getScore());
 						
 					}
 				}
-//				if (targeting.getBrandListCount() != 0) {
-//					for (int i = 0; i < targeting.getBrandListList().size(); i++) {
-//						if(targeting.getBrandListList().get(i).getBrandid()!=null&&targeting.getBrandListList().get(i).getScore()+""!="" ){
-//							pos.setFeature("targetingBrandid@"+targeting.getBrandListList().get(i).getBrandid(), targeting.getBrandListList().get(i).getScore());
-//							neg.setFeature("targetingBrandid@"+targeting.getBrandListList().get(i).getBrandid(), targeting.getBrandListList().get(i).getScore());
-//							
-//						}
-//					
-//						
-//					}
-//				}
-//				if (targeting.getSpecListCount() != 0) {
-//				for (int i = 0; i < targeting.getSpecListList().size(); i++) {
-//		
-//				
-//					pos.setFeature("targetingSpecid@"+targeting.getSpecListList().get(i).getSpecid(), targeting.getSpecListList().get(i).getScore());
-//					neg.setFeature("targetingSpecid@"+targeting.getSpecListList().get(i).getSpecid(), targeting.getSpecListList().get(i).getScore());
-//					
-//					
-//				}
-//			}
-//			if (targeting.getLevelListCount() != 0) {
-//				for (int i = 0; i < targeting.getLevelListList().size(); i++) {
-//	
-//
-//					pos.setFeature("targetingLevelid@"+targeting.getLevelListList().get(i).getLevelid(), targeting.getLevelListList().get(i).getScore());
-//					neg.setFeature("targetingLevelid@"+targeting.getLevelListList().get(i).getLevelid(), targeting.getLevelListList().get(i).getScore());
-//				}
-//			}
-//			if (targeting.getPriceListCount() != 0) {
-//				for (int i = 0; i < targeting.getPriceListList().size(); i++) {
-//
-//
-//					pos.setFeature("targetingPriceid@"+targeting.getPriceListList().get(i).getPriceid(), targeting.getPriceListList().get(i).getScore());
-//					neg.setFeature("targetingPriceid@"+targeting.getPriceListList().get(i).getPriceid(), targeting.getPriceListList().get(i).getScore());
-//				}
-//			}
-//			
-//			if (targeting.getExtendInfoListCount() != 0) {
-//				for (int i = 0; i < targeting.getExtendInfoListList().size(); i++) {
-//					pos.setFeature("targetingTagid@"+targeting.getExtendInfoListList().get(i).getTagid(), targeting.getExtendInfoListList().get(i).getScore());
-//					neg.setFeature("targetingTagid@"+targeting.getExtendInfoListList().get(i).getTagid(), targeting.getExtendInfoListList().get(i).getScore());
-//					
-//				}
-//			}
+				if (targeting.getBrandListCount() != 0) {
+					for (int i = 0; i < targeting.getBrandListList().size(); i++) {
+						if(targeting.getBrandListList().get(i).getBrandid()!=null&&targeting.getBrandListList().get(i).getScore()+""!="" ){						
+							sample.setFeature("targetingBrandid@"+targeting.getBrandListList().get(i).getBrandid(), targeting.getBrandListList().get(i).getScore());
+						}
+					
+					}
+				}
+				if (targeting.getSpecListCount() != 0) {
+				for (int i = 0; i < targeting.getSpecListList().size(); i++) {				
+					sample.setFeature("targetingSpecid@"+targeting.getSpecListList().get(i).getSpecid(), targeting.getSpecListList().get(i).getScore());
+					
+				}
+			}
+			if (targeting.getLevelListCount() != 0) {
+				for (int i = 0; i < targeting.getLevelListList().size(); i++) {	
+					sample.setFeature("targetingLevelid@"+targeting.getLevelListList().get(i).getLevelid(), targeting.getLevelListList().get(i).getScore());
+				}
+			}
+			if (targeting.getPriceListCount() != 0) {
+				for (int i = 0; i < targeting.getPriceListList().size(); i++) {
+					sample.setFeature("targetingPriceid@"+targeting.getPriceListList().get(i).getPriceid(), targeting.getPriceListList().get(i).getScore());
+				}
+			}
+		
+			if (targeting.getExtendInfoListCount() != 0) {
+				for (int i = 0; i < targeting.getExtendInfoListList().size(); i++) {
+					sample.setFeature("targetingTagid@"+targeting.getExtendInfoListList().get(i).getTagid(), targeting.getExtendInfoListList().get(i).getScore());
+				}
+			}
 				
 			}
 			
-			HashSet<String> saleleads_set = new HashSet<String>();
+			if(behaviorList !=null&&behaviorList.size() !=0){
+				for(BehaviorInfoOperation.BehaviorInfo behavior:behaviorList ){
+			
+					sample.setFeature("behaviorSaleleadstype@"+behavior.getSaleleadstype());
+					sample.setFeature("behaviorBrandid1@"+behavior.getBrandid1());
+					sample.setFeature("behaviorBrandid@"+behavior.getBrandid2());
+					sample.setFeature("behaviorBrandid3@"+behavior.getBrandid3());
+					sample.setFeature("behaviorBrandid4@"+behavior.getBrandid4());
+					sample.setFeature("behaviorSeriesid1@"+behavior.getSeriesid1());
+					sample.setFeature("behaviorSeriesid2@"+behavior.getSeriesid2());
+					sample.setFeature("behaviorSeriesid3@"+behavior.getSeriesid3());
+					sample.setFeature("behaviorSeriesid4@"+behavior.getSeriesid4());
+					sample.setFeature("behaviorSpecid1@"+behavior.getSpecid1());
+					sample.setFeature("behaviorSpecid2@"+behavior.getSpecid2());
+					sample.setFeature("behaviorSpecid3@"+behavior.getSpecid3());
+					sample.setFeature("behaviorSpecid4@"+behavior.getSpecid4());
+					sample.setFeature("behaviorUrldealerid@"+behavior.getUrldealerid());
+					sample.setFeature("behaviorUrljbid@"+behavior.getUrljbid());
+					sample.setFeature("behaviorUrlprice@"+behavior.getUrlprice());
+				}
+			}
+			
+
 			
 			
 			if (saleleadsList != null && saleleadsList.size() != 0) {
 				for (SaleleadsInfoOperation.SaleleadsInfo saleleads : saleleadsList) {
 					
-					if(saleleads_set.contains(saleleads.getCururl() + "\t" + saleleads.getRefurl()))
-						continue;
-					else
-						saleleads_set.add(saleleads.getCururl() + "\t" + saleleads.getRefurl());
-					
-				//	context.write(new Text(tags), new Text("1"));
-					context.write( NullWritable.get(), pos);
+
+
+					sample.setLabel(1.0);
+					context.write( NullWritable.get(), sample);
 				}
 			} else if (pvList != null && pvList.size() != 0) {
 				for (PvlogOperation.AutoPVInfo pvinfo : pvList) {
+					
 
 				}
 
-				//context.write(new Text(tags), new Text("0"));
-				context.write( NullWritable.get(), neg);
+
+
+				sample.setLabel(0.0);
+				context.write( NullWritable.get(), sample);
 			} else if (app_pvList != null && app_pvList.size() != 0) {
 				for (ApplogOperation.AutoAppInfo apppvinfo : app_pvList) {
 
 				}
-				
-				//context.write(new Text(tags), new Text("0"));
-				context.write( NullWritable.get(), neg);
+
+				sample.setLabel(0.0);
+				context.write( NullWritable.get(), sample);
 			}
 		
 			
