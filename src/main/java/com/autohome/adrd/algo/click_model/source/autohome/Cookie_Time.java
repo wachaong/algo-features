a826c890c4d3ad7f6dd9c6389306406c87b47cfc
unknown <Administrator@MININT-9EMIU1F.(none)>
2014-11-04 09:51:24 +0800

    add user's login time

--- src/main/java/com/autohome/adrd/algo/click_model/source/autohome/Cookie_Time.java
+++ src/main/java/com/autohome/adrd/algo/click_model/source/autohome/Cookie_Time.java
@@ -16,6 +16,7 @@ import java.util.regex.Pattern;
 import org.apache.hadoop.hive.serde2.columnar.BytesRefArrayWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer.Context;
 import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 import org.apache.hadoop.mapreduce.InputSplit;
 import org.apache.hadoop.mapreduce.Job;
@@ -31,12 +32,11 @@ import com.autohome.adrd.algo.protobuf.SaleleadsInfoOperation;
 import com.autohome.adrd.algo.protobuf.TargetingKVOperation;
 
 /**
- * 
- * @author [Wangchao: wangchao@autohome.com.cn ]
- * 
+ * 用户的注册时长[以天为单位]
+ * @author : Yang Mingmin 
  */
 
-public class CookieTime extends AbstractProcessor {
+public class Cookie_Time extends AbstractProcessor {
 
 	public static class RCFileMapper extends RCFileBaseMapper<Text, Text> {
 
@@ -47,40 +47,56 @@ public class CookieTime extends AbstractProcessor {
 		public static final String CG_TAGS = "tags";
 		public static final String CG_APPPV = "apppv";
 		public static final String CG_BEHAVIOR = "behavior";
-
+		
+		private static String pred_date;
+		
 		public void setup(Context context) throws IOException, InterruptedException {
 			super.setup(context);
 			projection = context.getConfiguration().get("mapreduce.lib.table.input.projection", "user,behavior,tags,addisplay,adclick,pv");
+			pred_date = context.getConfiguration().get("pred_date"); 		
 		}
 
 		@SuppressWarnings({ "unchecked", "deprecation" })
 		public void map(LongWritable key, BytesRefArrayWritable value, Context context) throws IOException, InterruptedException {
-			List<PvlogOperation.AutoPVInfo> pvList = new ArrayList<PvlogOperation.AutoPVInfo>();
-			
-			Pattern pattern = Pattern.compile("^[-\\+]?[\\d]*$");
 			decode(key, value);
-
-			pvList = (List<PvlogOperation.AutoPVInfo>) list.get(CG_PV);
 			String cookie = (String) list.get("user");
-			
 			TargetingKVOperation.TargetingInfo targeting = (TargetingKVOperation.TargetingInfo) list.get(CG_TAGS);
 			
-			
-			if (targeting != null) {
+ 		    if (targeting != null) {
 				String cookie_time = targeting.getCookieTime();
-				context.write(new Text(cookie), new Text(cookie_time));
-
-			}
-																		
+				if(cookie_time == null || cookie_time.trim() == "0")
+					return;
+				String date = cookie_time.trim().split(" ")[0].trim().replaceAll("-", "");
+				Date d;
+				try {
+					d = new SimpleDateFormat("yyyyMMdd").parse(date);
+					Date d2 = new SimpleDateFormat("yyyyMMdd").parse(pred_date);
+					long diff = d2.getTime() - d.getTime();
+					Long days = diff/(1000*60*60*24);
+					context.write(new Text(cookie), new Text(days.toString()));
+				}catch (ParseException e) {
+					e.printStackTrace();
+				}	
+				
+			}														
 		}
 	}
 
-
+    public static class HReduce extends Reducer<Text, Text, Text, Text> {
+    	public void reduce(Text key, Iterable<Text> values, Context context) throws 
+    	    IOException, InterruptedException {
+    		for(Text value : values) {
+    			context.write(key, value);
+    			return;
+    		}
+    	}
+    }
+    
 	@Override
 	protected void configJob(Job job) {
 		job.getConfiguration().set("mapred.job.priority", "VERY_HIGH");
 		job.setMapperClass(RCFileMapper.class);
-		//job.setReducerClass(HReduce.class);
+		job.setReducerClass(HReduce.class);
 		job.setMapOutputKeyClass(Text.class);
 		job.setMapOutputValueClass(Text.class);
 		job.setOutputKeyClass(Text.class);
---
+++ src/main/java/com/autohome/adrd/algo/click_model/source/autohome/HistoryClick.java
@@ -0,0 +1,132 @@
+package com.autohome.adrd.algo.click_model.source.autohome;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+import org.apache.hadoop.hive.serde2.columnar.BytesRefArrayWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import com.autohome.adrd.algo.sessionlog.consume.RCFileBaseMapper;
+import com.autohome.adrd.algo.click_model.io.AbstractProcessor;
+import com.autohome.adrd.algo.protobuf.AdLogOperation;
+import com.autohome.adrd.algo.protobuf.ApplogOperation;
+import com.autohome.adrd.algo.protobuf.PvlogOperation;
+import com.autohome.adrd.algo.protobuf.PvlogOperation.AutoPVInfo;
+import com.autohome.adrd.algo.protobuf.SaleleadsInfoOperation;
+import com.autohome.adrd.algo.protobuf.SaleleadsInfoOperation.SaleleadsInfo;
+import com.autohome.adrd.algo.protobuf.TargetingKVOperation;
+
+/**
+ * 
+ * @author [Wangchao: wangchao@autohome.com.cn ]
+ * 
+ */
+
+public class HistoryClick extends AbstractProcessor {
+
+	public static class RCFileMapper extends RCFileBaseMapper<Text, Text> {
+
+		public static final String CG_USER = "user";
+		public static final String CG_PV = "pv";
+		public static final String CG_SEARCH = "search";
+		public static final String CG_SALE_LEADS = "saleleads";
+		public static final String CG_TAGS = "tags";
+		public static final String CG_APPPV = "apppv";
+		public static final String CG_BEHAVIOR = "behavior";
+		
+		private static String pred_date;
+		private static double decay;
+
+		public void setup(Context context) throws IOException, InterruptedException {
+			super.setup(context);
+			projection = context.getConfiguration().get("mapreduce.lib.table.input.projection", "user,addisplay,adclick");
+			pred_date = context.getConfiguration().get("pred_date");
+			decay = context.getConfiguration().getDouble("decay",0.8);
+		}
+
+		@SuppressWarnings({ "unchecked", "deprecation" })
+		public void map(LongWritable key, BytesRefArrayWritable value, Context context) throws IOException, InterruptedException {
+			
+			List<SaleleadsInfoOperation.SaleleadsInfo> saleleadsList = new ArrayList<SaleleadsInfoOperation.SaleleadsInfo>();
+			List<PvlogOperation.AutoPVInfo> pvList = new ArrayList<PvlogOperation.AutoPVInfo>();
+			
+			Pattern pattern = Pattern.compile("^[-\\+]?[\\d]*$");
+			decode(key, value);
+
+			saleleadsList = (List<SaleleadsInfoOperation.SaleleadsInfo>) list.get(CG_SALE_LEADS);
+			pvList = (List<PvlogOperation.AutoPVInfo>) list.get(CG_PV);
+
+			String cookie = (String) list.get("user");
+			
+			String path=((FileSplit)context.getInputSplit()).getPath().toString();
+			String date = path.split("sessionlog")[1].split("part")[0].replaceAll("/", "");
+			Date d;
+			try {
+				d = new SimpleDateFormat("yyyyMMdd").parse(date);
+				Date d2 = new SimpleDateFormat("yyyyMMdd").parse(pred_date);
+				long diff = d2.getTime() - d.getTime();
+				long days = diff/(1000*60*60*24);
+				
+				int saleleads_cnt = 0, pv_cnt = 0;
+				if (saleleadsList != null && saleleadsList.size() != 0)
+					saleleads_cnt = saleleadsList.size();
+				if (pvList != null && pvList.size() != 0)
+					pv_cnt = pvList.size();
+				
+				double score_total = 0;
+				if(saleleads_cnt > 0 && pv_cnt > 0)
+				{
+					for(SaleleadsInfo pvinfo : saleleadsList)
+					{
+						double score = Math.pow(decay,days);
+						score_total += score;
+					}
+					context.write(new Text(cookie), new Text(String.valueOf(score_total)));
+				}
+				
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+																		
+		}
+	}
+
+	public static class HReduce extends Reducer<Text, Text, Text, Text> {
+
+		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+
+			double score_total = 0.0;
+			for (Text value : values) {
+				score_total += Double.valueOf(value.toString());
+			}			
+			context.write(key, new Text(String.valueOf(score_total)));
+		}
+	}
+
+	@Override
+	protected void configJob(Job job) {
+		job.getConfiguration().set("mapred.job.priority", "VERY_HIGH");
+		job.setMapperClass(RCFileMapper.class);
+		job.setReducerClass(HReduce.class);
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(Text.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(Text.class);
+	}
+}
